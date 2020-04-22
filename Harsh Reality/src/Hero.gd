b721fec50286f83a37e828df972a0fa0aa4c8ae0
Touko <toukkeli@hotmail.com>
2020-04-22 20:49:49 +0300

    Added the new animation to the player (broken, black box)

--- Harsh Reality/src/Hero.gd
+++ Harsh Reality/src/Hero.gd
@@ -3,6 +3,10 @@ var direction: = Vector2(-1.0,0.0)
 
 func _physics_process(delta):
 	direction = get_direction(direction)
+	if direction.x == 1.0: 
+		get_node("Sprite").flip_h = false 
+	else:  
+		get_node("Sprite").flip_h = true
 	is_jump_interrupted = Input.is_action_just_released("move_jump")
 	velocity = calculate_move_velocity(velocity, direction, speed, is_jump_interrupted)
 
