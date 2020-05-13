3fc54037836e3f779c0568a248ae8ff4509e24c0
Touko <toukkeli@hotmail.com>
2020-05-13 19:25:00 +0300

    Changes to default level

--- Harsh Reality/src/actor.gd
+++ Harsh Reality/src/actor.gd
@@ -1,16 +1,16 @@
 extends KinematicBody
 class_name Actor
 
-export var speed: = Vector3(200.0, 500.0, 0.0)
-export var maxspeed: = Vector3(400.0, 1000.0, 0.0)
-export var gravity: = 400.0
+export var speed: = Vector3(20.0, 50.0, 0.0)
+export var maxspeed: = Vector3(20.0, 1000.0, 0.0)
+export var gravity: = 20.0
 export var is_jump_interrupted: = false
 var velocity: = Vector3.ZERO
 const FLOOR_NORMAL: = Vector3.UP
 
 
 func _physics_process(delta):
-	velocity = move_and_slide(velocity, FLOOR_NORMAL, 0.0)
+	velocity = move_and_slide(velocity, FLOOR_NORMAL)
 	
 
 func calculate_move_velocity(
@@ -27,9 +27,9 @@ func calculate_move_velocity(
 #			new_velocity.x =  max(new_velocity.x - speed.x, 0.0)
 #		else:
 #			new_velocity.x =  min(new_velocity.x + speed.x, 0.0)
-	new_velocity.y += gravity * get_physics_process_delta_time()
+	new_velocity.y += -gravity * get_physics_process_delta_time()
 	if direction.y == -1.0:
-		new_velocity.y = speed.y * direction.y
+		new_velocity.y = -speed.y * direction.y
 	if is_jump_interrupted:
 		new_velocity.y = max(new_velocity.y, 0.0)
 	return new_velocity
