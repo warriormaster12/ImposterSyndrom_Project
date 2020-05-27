1f0f4810ecf1fa99c5b6c83398d3d93a38870195
warriormaster12 <streng.alexander@gmail.com>
2020-05-27 19:57:50 +0300

    fixed flipping

--- Harsh Reality/src/player.gd
+++ Harsh Reality/src/player.gd
@@ -9,10 +9,13 @@ func _physics_process(delta):
 	else: 
 		anim.play("Idle")
 	
-	if direction.x == -1:
-		anim.flip_h=true
-	else:
+	
+	if Input.is_action_pressed("move_right"):
 		anim.flip_h=false
+	if Input.is_action_pressed("move_left"):
+		anim.flip_h=true
+
+
 	
 
 	
@@ -26,4 +29,4 @@ func get_direction() -> Vector3:
 		-1.0 if Input.is_action_just_pressed("move_jump") and is_on_floor() else 1.0
 		, 0.0
 	)
-
+	
