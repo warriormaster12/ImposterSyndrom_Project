603a9a50c11a01db4317b2df9ca108b3c8fa0311
warriormaster12 <streng.alexander@gmail.com>
2020-05-27 19:30:58 +0300

    Player can look at different directions

--- Harsh Reality/src/player.gd
+++ Harsh Reality/src/player.gd
@@ -2,10 +2,20 @@ extends Actor
 
 onready var anim: = get_node("AnimatedSprite3D")
 
+
 func _physics_process(delta):
 	var direction: = get_direction()
 	if direction.x != 0.0: anim.play("Run") 
-	else: anim.play("Idle")
+	else: 
+		anim.play("Idle")
+	
+	if direction.x == -1:
+		anim.flip_h=true
+	else:
+		anim.flip_h=false
+	
+
+	
 	is_jump_interrupted = Input.is_action_just_released("move_jump")
 	velocity = calculate_move_velocity(velocity, direction, speed, is_jump_interrupted)
 
